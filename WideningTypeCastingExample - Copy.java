63db32db97761930009787c03b594db783a19917
Palak-0301 <125293499+Palak-0301@users.noreply.github.com>
2023-08-01 13:54:47 +0530

    Java Programs

---
+++ WideningTypeCastingExample - Copy.java
@@ -0,0 +1,15 @@
+public class Wideningtypecastingexample {
+    public static void main(String[] args)
+{
+int x =25;
+//automatically converts the integer type into long type long;
+ y = x;
+//automatically converts the long type into float type float;
+ z = y;
+System.out.println("Before conversion, int value "+x); 
+System.out.println("After conversion, long value "+y);
+System.out.println("After conversion, float value "+z); 
+System.out.println("Name= Palakpreet Kaur"+'\n'+"Class = D3ITB2");
+}
+
+}
---
+++ typecasting.java
@@ -0,0 +1,12 @@
+public class typecasting {
+    public static void main(String[] args) { 
+        System.out.println("Name: Palakpreet Kaur\n URN: 2004836\n CRN: 2021144\n Class: ITB2 ");
+         double myDouble = 9.78d; 
+         int myInt = (int) myDouble; // Explicit casting: double to int 
+      
+         System.out.println(myDouble); 
+         System.out.println(myInt); 
+       } 
+     
+        
+}
